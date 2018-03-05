0269e3072cc5d9f05d969c6576afa01d5064a474
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-03-05 22:37:09 +0300

    react rediux

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { decrement, decrementAsync, increment } from '../../modules/counter';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
-const About = () => (
+
+const About = (props) => (
   <div>
+    <p>Count: {props.count}</p>
     <h1>About Page</h1>
     <p>Did you get here via Redux?</p>
   </div>
 );
 
-export default About;
+const mapStateToProps = state => ({
+  count: state.counter.count
+});
+
+export default connect(mapStateToProps)(About);
